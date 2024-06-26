8f6cdecda558d0f2f748f733ad563e45f78b1db1
Ganesh-2134 <ganeshkumar_smmkg@yahoo.com>
2024-06-26 20:41:26 +0530

    updated s3 to save state file

--- provider.tf
+++ provider.tf
@@ -1,3 +1,19 @@
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 4.16"
+#     }
+#   }
+
+#   required_version = ">= 1.2.0"
+# }
+
+# provider "aws" {
+#   access_key = var.aws_access_key
+#   secret_key = var.aws_secret_key
+#   region     = "us-east-1"
+# }
 terraform {
   required_providers {
     aws = {
@@ -7,6 +23,13 @@ terraform {
   }
 
   required_version = ">= 1.2.0"
+
+  backend "s3" {
+    bucket         = "autodesk-project" 
+    key            = "terraform.tfstate"  
+    region         = "us-east-1"            
+    encrypt        = true
+  }
 }
 
 provider "aws" {
