eb274a4878c50d3fb73b113fa2e56c1bec680ee0
Ganesh-2134 <ganeshkumar_smmkg@yahoo.com>
2024-06-27 01:08:38 +0530

    code updated

--- alb.tf
+++ alb.tf
@@ -17,6 +17,7 @@ resource "aws_lb_target_group" "main" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  target_type = "ip"  # or "ip\_address"
 
   health_check {
     path                = "/"
@@ -44,19 +45,6 @@ resource "aws_lb_listener" "http" {
   }
 }
 
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = "443"
-  protocol          = "HTTPS"
-
-  ssl_policy      = "ELBSecurityPolicy-2016-08"
-  certificate_arn = var.certificate_arn
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.main.arn
-  }
-}
 
 output "lb_dns_name" {
   description = "The DNS name of the load balancer"
